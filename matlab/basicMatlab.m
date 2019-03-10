86cae31788abfd336410349d0999e509787c2a2e
Eric Lee <Raidenlkl@gmail.com>
2019-03-10 21:28:53 +1000

    matlab basic notes

---
+++ matlab/basicMatlab.m
@@ -0,0 +1,17 @@
+%% Open a Cell - This Section does some simple arithematic
+
+% Edit to open editor
+
+%{
+    All Commend
+    It is disable to use
+%}
+
+
+
+% It means we are writing commend
+52 + 7 + ... % ...means keep going to next line
+    118
+%% Next Section
+
+%%
